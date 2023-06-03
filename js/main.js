59ec9a4f1e483cc1168307140ca73a431860d106
MaxEfuru <fredrickmaximum@gmail.com>
2023-06-03 22:03:53 +0100

    latest update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 "use strict";
+let col = sessionStorage.getItem("bgColor") || "fff";  // bg color variable
+document.body.style.background ="#"+col;
+
 const select__utility = document.getElementById("select__utility");
 const variant         = document.getElementById("variant");
 
@@ -9,9 +12,9 @@ select__utility.addEventListener("change", function(evt){
       
     switch(evt.target.value){
         case "airtime":
-            variant_option = `<option value="AIRTEL">Airtel Airtime</option>
-                              <option value="MTN"> MTN Airtime</option>
-                              <option value="GLO"> GLO Airtime</option>
+            variant_option = `<option value="AIRTEL"> Airtel Airtime </option>
+                              <option value="MTN"> MTN Airtime </option>
+                              <option value="GLO"> GLO Airtime </option>
                               <option value="9MOBILE"> 9Mobile Airtime </option>
                             `;
                             // variant.innerHTML = variant_option; 
@@ -58,7 +61,7 @@ select__utility.addEventListener("change", function(evt){
 
 document.querySelectorAll('div.links a').forEach((a)=>{
     a.addEventListener("click", function(e){
-         e.preventDefault();
+        //  e.preventDefault();
          const link = e.target; 
          // remove all active classes
          removeActiveClass( document.querySelectorAll('div.links a') ); 
@@ -67,10 +70,12 @@ document.querySelectorAll('div.links a').forEach((a)=>{
 
          if(link.classList.contains("link-a")){
             const id = link.getAttribute('href').split("#")[1];
+            // const id2 = location.hash;
+            // console.log(id, id2);
             if(id !== undefined){
                 document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
             }
-            console.log(id);
+           
          }
     });
 });
@@ -89,15 +94,85 @@ function removeActiveClass(list){
 
 // the bottom button
 let bottom_btn = document.querySelector(".up__arrow");
-bottom_btn.addEventListener("click",function(e){
-    console.log(this);
+bottom_btn.addEventListener("click",function(e){ 
      let color = Math.floor( Math.random() * 1000 );
-    //  console.log(color);
-    document.body.style.background ="#"+color;
+     sessionStorage.setItem("bgColor", color);
+     
+     if( sessionStorage.getItem("bgColor") ){
+        col = sessionStorage.getItem("bgColor");
+        document.body.style.background ="#"+col;
+     }else{
+        document.body.style.background ="#"+color;
+     } 
+    
 });
 
-var amira = 0;
-const amia = 10;
 
-console.log(window);
-console.log(this);
+/// WOrking with the STORAGE API
+
+//localStorage
+localStorage.setItem("myStore","GoldStock");
+// localStorage.setItem("myArrStore",[2,4,6,7,9,12,34,3]);
+// localStorage.setItem("myObjectStore",{"name":"GoldStock","worth":"200BTC"});
+localStorage.setItem("myArrStore",JSON.stringify([2,4,6,7,9,12,34,3]));
+localStorage.setItem("myObjectStore",JSON.stringify({"name":"GoldStock","worth":"200BTC"}) );
+
+let store = localStorage.getItem("myStore");
+let myObj = JSON.parse( localStorage.getItem("myObjectStore") );
+
+console.log(myObj);
+console.log(myObj.name);
+
+
+console.log( typeof localStorage.getItem("myArrStore") );
+console.log(  JSON.parse( localStorage.getItem("myArrStore") ) );
+
+console.log(store);
+
+
+//sessionStorage
+// sessionStorage.setItem("myStore2","GoldStock2");
+
+
+let mystr = "I am welcome to this place";
+let part  = mystr.substring(5,9);
+console.log(part);
+
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
+  }
+
+  checkCookie();
+
